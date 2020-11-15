2c4590579bb7b5b301032db254f558a802e23b32
Gareth Ferguson <gareth.f@hotmail.co.uk>
2020-11-15 19:58:20 +0000

    Fix Gatsby globals

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -16,6 +16,10 @@ global.___loader = {
     enqueue: () => {},
     hovering: () => {},
 }
+
+global.__PATH_PREFIX__ = ``
+global.BASE_PATH__ = ``
+
 // Navigating through a gatsby app using gatsby-link or any other gatsby component will use the `___navigate` method.
 // In Storybook it makes more sense to log an action than doing an actual navigate. Checkout the actions addon docs for more info: https://github.com/storybookjs/storybook/tree/master/addons/actions.
 window.___navigate = pathname => {
