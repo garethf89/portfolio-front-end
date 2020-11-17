544e8a09a3ba27e84013f608b0203959c685bc37
Gareth Ferguson <gareth.f@hotmail.co.uk>
2020-11-17 21:04:32 +0000

    Fix storybook base path

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -18,7 +18,7 @@ global.___loader = {
 }
 
 global.__PATH_PREFIX__ = ``
-global.BASE_PATH__ = ``
+global.__BASE_PATH__ = ``
 
 // Navigating through a gatsby app using gatsby-link or any other gatsby component will use the `___navigate` method.
 // In Storybook it makes more sense to log an action than doing an actual navigate. Checkout the actions addon docs for more info: https://github.com/storybookjs/storybook/tree/master/addons/actions.
