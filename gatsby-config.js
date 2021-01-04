634541733d907272c7e4978c9a30589714957db6
Gareth Ferguson <gareth.f@hotmail.co.uk>
2021-01-04 00:03:34 +0000

    add a sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,6 +43,7 @@ module.exports = {
     },
     plugins: [
         "gatsby-plugin-robots-txt",
+        "gatsby-plugin-sitemap",
         `gatsby-plugin-react-helmet`,
         {
             resolve: `gatsby-source-filesystem`,
