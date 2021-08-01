92d8d45458c33d53a28098016bc6c220f94f3ae1
Gareth Ferguson <gareth.f@hotmail.co.uk>
2021-08-01 12:26:12 +0100

    update storybook and remove the docgen

--- .storybook/main.js
+++ .storybook/main.js
@@ -20,7 +20,7 @@ module.exports = {
     typescript: {
         check: false,
         checkOptions: {},
-        reactDocgen: "react-docgen-typescript",
+        reactDocgen: false,
         reactDocgenTypescriptOptions: {
             shouldExtractLiteralValuesFromEnum: true,
             propFilter: prop =>
