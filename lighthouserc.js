6f6023aab2bd1cbe3cc7bd79e9ae93387831dc32
Gareth Ferguson <gareth.f@hotmail.co.uk>
2022-04-05 12:02:36 +0100

    actions for lighthouse

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,12 +2,11 @@ module.exports = {
     ci: {
         collect: {
             startServerCommand: "npm start",
+            staticDistDir: "./public",
+
         },
         upload: {
             target: "temporary-public-storage",
         },
-    },
-    collect: {
-        staticDistDir: "./public",
     },
 }
