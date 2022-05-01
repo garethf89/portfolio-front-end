e8bf80a1e1260b1a8bbb3fa814ed7bcc2af644d1
Gareth Ferguson <gareth.f@hotmail.co.uk>
2022-05-01 12:43:30 +0100

    fix the errors on svgs

--- src/gatsby/node/inline-svg.js
+++ src/gatsby/node/inline-svg.js
@@ -10,54 +10,11 @@ const queue = new PQueue({
 
 const svgoConfig = {
     multipass: true,
-    floatPrecision: 2,
     plugins: [
-        { name: "removeDoctype" },
-        { name: "removeXMLProcInst" },
-        { name: "removeComments" },
-        { name: "removeMetadata" },
-        { name: "removeEditorsNSData" },
-        { name: "cleanupAttrs" },
-        { name: "inlineStyles" },
-        { name: "minifyStyles" },
-        { name: "convertStyleToAttrs" },
-        { name: "cleanupIDs" },
-        { name: "prefixIds" },
-        { name: "removeRasterImages" },
-        { name: "removeUselessDefs" },
-        { name: "cleanupNumericValues" },
-        { name: "cleanupListOfValues" },
-        { name: "convertColors" },
-        { name: "removeUnknownsAndDefaults" },
-        { name: "removeNonInheritableGroupAttrs" },
-        { name: "removeUselessStrokeAndFill" },
-        { name: "removeViewBox" },
-        { name: "cleanupEnableBackground" },
-        { name: "removeHiddenElems" },
-        { name: "removeEmptyText" },
-        { name: "convertShapeToPath" },
-        { name: "moveElemsAttrsToGroup" },
-        { name: "moveGroupAttrsToElems" },
-        { name: "collapseGroups" },
-        { name: "convertPathData" },
-        { name: "convertTransform" },
-        { name: "removeEmptyAttrs" },
-        { name: "removeEmptyContainers" },
-        { name: "mergePaths" },
-        { name: "removeUnusedNS" },
-        { name: "sortAttrs" },
-        { name: "removeTitle" },
-        { name: "removeDesc" },
-        { name: "removeDimensions"},
-        { name: "removeAttrs" },
-        { name: "removeAttributesBySelector" },
-        { name: "removeElementsByAttr" },
-        { name: "addClassesToSVGElement" },
-        { name: "removeStyleElement" },
-        { name: "removeScriptElement" },
-        { name: "addAttributesToSVGElement" },
-        { name: "removeOffCanvasPaths" },
-        { name: "reusePaths" },
+        {
+            name: "preset-default",
+        },
+        { name: "removeDimensions" },
     ],
 }
 
@@ -122,50 +79,6 @@ exports.createSchemaCustomization = ({ actions }) => {
     updatedAt: String
   }
 `)
-}
-
-async function parseSVG({
-    source,
-    uri,
-    store,
-    cache,
-    createNode,
-    createNodeId,
-}) {
-    // Get remote file
-    const { absolutePath } = await createRemoteFileNode({
-        url: uri,
-        parentNodeId: source.id,
-        store,
-        cache,
-        createNode,
-        createNodeId,
-    })
-
-    // Read local file
-    const svg = await fs.readFile(absolutePath)
-
-    if (!svg) {
-        throw new Error(
-            "Unable to read " + source.contentful_id + ": " + absolutePath
-        )
-    }
-
-    // Optimize
-    if (svg.indexOf("base64") !== -1) {
-        console.log(
-            "SVG contains pixel data. Pixel data was removed to avoid file size bloat.",
-            source.contentful_id + ": " + absolutePath
-        )
-    }
-    const { data: optimizedSVG } = await optimize(svg, {
-        path: absolutePath,
-    })
-
-    return {
-        content: optimizedSVG,
-        updatedAt: source.updatedAt,
-    }
 }
 
 exports.createResolvers = ({
