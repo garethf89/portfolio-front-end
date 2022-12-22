a7b6b7ed5cc247b5ae2c7a749429ace6bdc4f122
Gareth Ferguson <gareth.f@hotmail.co.uk>
2022-12-22 10:57:58 +0000

    Correct ENV variable

--- src/constants/lastfm.ts
+++ src/constants/lastfm.ts
@@ -1,3 +1,3 @@
 export const data = { name: "Gazmatron1" }
 export const url = `${process.env.NEXT_PUBLIC_APP_API_URL}/lastFm`
-export const functionGet = `${process.env.NEXT_PUBLIC_APP_FE_URL}/.netlify/functions/lastFM`
+export const functionGet = `${process.env.NEXT_PUBLIC_REACT_APP_FE_URL}/.netlify/functions/lastFM`
