3e7cb826ad9f80204e13049969e758a387d1773f
Gareth Ferguson <gareth.f@hotmail.co.uk>
2023-06-09 13:21:18 +0100

    Update Button.tsx

--- src/components/Common/Button.tsx
+++ src/components/Common/Button.tsx
@@ -48,9 +48,8 @@ const renderButton = (icon: ButtonTypes) => {
 
 export type ButtonTypes = "Download" | "Arrow"
 
-type ButtonProps =
-    | ({
-          icon?: ButtonTypes
+type ButtonProps = {
+        icon?: ButtonTypes
           type?: "button" | "submit" | "reset"
           children: React.ReactElement | string
           color?: string
@@ -61,8 +60,8 @@ type ButtonProps =
           as?: React.ElementType
           variant: string
           download?: boolean
-      } & React.ButtonHTMLAttributes<HTMLButtonElement>)
-    | React.AnchorHTMLAttributes<HTMLAnchorElement>
+      } & (React.ButtonHTMLAttributes<HTMLButtonElement>
+    | React.AnchorHTMLAttributes<HTMLAnchorElement>)
 
 const Border = styled("svg")`
     position: absolute;
